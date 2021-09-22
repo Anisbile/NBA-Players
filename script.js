4717995abfe0db8023265b0f87fc644945a02a20
Anis Bile <anisbile@Aniss-MacBook-Air.local>
2021-09-22 00:34:23 -0500

    wip: adds updated script.js and index.html

---
+++ script.js
@@ -0,0 +1,37 @@
+$(function(){
+    const BASE_URL = 'https://www.balldontlie.io';
+    
+
+    
+    let playerData;
+    
+    const $first_name = $('#first'); 
+    const $last_name= $('#last');
+    const $position = $('#position');
+    const $form = $('form'); 
+    const $input = $('input[type="text"]');
+    
+    $form.on('submit', handleGetData)
+    
+
+    function handleGetData(event) {
+        event.preventDefault(); 
+        const playerTitle = $input.val();
+        $input.val("");
+        $.ajax(`${BASE_URL}/api/v1/players/${playerTitle}`).then(function (data) {
+            playerData = data;
+            render();
+        }, function (error) {
+            console.log(error);
+        });
+    }
+    
+    
+    function render() {
+        $first_name.text(playerData.FirstName);
+        $last_name.text(playerData.LastName);
+        $position.text(playerData.Position);
+    }
+    
+        
+    });
